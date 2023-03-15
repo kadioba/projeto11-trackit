ba8f48566a47a601a9b5594c5a76c7cf282a5875
Vinicius Predebon <kadiobaoff@gmail.com>
2023-03-15 17:06:56 -0300

    Versao com tela de cadastro finalizada

--- src/components/Cadastro/Cadastro.js
+++ src/components/Cadastro/Cadastro.js
@@ -1,21 +1,77 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
+
+import React from 'react'
+import axios from "axios";
+import { ThreeDots } from 'react-loader-spinner'
+
+
 
 import logoGrande from "../../assets/logo-grande.png"
 
 export default function Cadastro() {
+
+    const [email, setEmail] = React.useState("");
+    const [senha, setSenha] = React.useState("");
+    const [nome, setNome] = React.useState("");
+    const [foto, setFoto] = React.useState("");
+    const [cadastrar, setCadastrar] = React.useState(false)
+
+    const navigate = useNavigate()
+
+    function cadastrarUsuario(event) {
+
+        event.preventDefault();
+
+        const requisicao = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up", {
+            email: email,
+            name: nome,
+            image: foto,
+            password: senha
+        })
+
+        setCadastrar(true);
+
+        requisicao.then(resposta => {
+            console.log("Sucesso")
+            console.log(resposta)
+            navigate("/")
+        })
+
+        requisicao.catch(resposta => {
+            console.log("Erro")
+            console.log(resposta)
+            setCadastrar(false)
+            alert("Erro ao criar conta, tente novamente")
+        })
+
+    }
+
     return (
         <PaginaDeCadastro>
             <img src={logoGrande} alt="" />
             <div>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <button>Entrar</button>
+                <form onSubmit={cadastrarUsuario}>
+                    <input disabled={cadastrar} placeholder="email" type="email" value={email} onChange={e => setEmail(e.target.value)} />
+                    <input disabled={cadastrar} placeholder="senha" type="password" value={senha} onChange={e => setSenha(e.target.value)} />
+                    <input disabled={cadastrar} placeholder="nome" type="text" value={nome} onChange={e => setNome(e.target.value)} />
+                    <input disabled={cadastrar} placeholder="foto" type="url" value={foto} onChange={e => setFoto(e.target.value)} />
+                    <button disabled={cadastrar} type="submit" >{cadastrar ? <ThreeDots
+                        text
+                        height="80"
+                        width="80"
+                        radius="9"
+                        color="white"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{}}
+                        wrapperClassName=""
+                        visible={true}
+                    /> : "Cadastrar"}</button>
+                </form>
             </div>
             <Link to="/">Já tem uma conta? Faça login!</Link>
-        </PaginaDeCadastro>
+        </PaginaDeCadastro >
     )
 }
 
@@ -23,7 +79,7 @@ const PaginaDeCadastro = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-div{
+form{
     margin-top: 32px;
     width: 303px;
     height: 249px;
@@ -52,6 +108,9 @@ button{
     border-radius: 4.63636px;
     color: #FFFFFF;
     border: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 a{
     margin-top: 25px;
